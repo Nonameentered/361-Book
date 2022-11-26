efb9981cdd6f0276ca9795f3edf34b416e682584
Matt Shu <matthewshu88@gmail.com>
2022-11-25 22:10:24 -0500

    update broken commands

--- _deploy.sh
+++ _deploy.sh
@@ -14,4 +14,4 @@ cp -r ../_book/* ./
 git add --all *
 git commit -m "Update the book" || true
 git remote add origin-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git > /dev/null 2>&1
-git push -q origin-pages gh-pages
+git push --quiet --set-upstream origin-pages gh-pages
